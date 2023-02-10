1c1f1e39613419596cd74cda2786e0759831c2bd
lipeharakawa <felipeharakawa01@gmail.com>
2023-02-09 23:11:01 -0300

    evoluindo

--- script.js
+++ script.js
@@ -135,7 +135,138 @@ let mensagem2 = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de id
 
 console.log(mensagem1);
 console.log(mensagem2);
+
+//Desconstruindo objetos
+let pessoa = {
+    nome:"Matheus",
+    sobrenome: "Fraga",
+    empresa: "Sujeito Programador",
+    cargo:"Programador FullStack"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.cargo);
+
+let nome = "Felipe";
+
+const { nome:nomePessoa, cargo } = pessoa;
+
+console.log(nomePessoa);
+console.log(cargo);
+
+//Desconstruindo array
+let nomes = ["Matheus", "Lucas", "Henrique"];
+
+console.log(nomes);
+console.log(nomes[2]);
+
+let {0:primeiraPessoa, 2:henrique} = nomes;
+
+console.log(primeiraPessoa);
+console.log(henrique);
+
+let [primeiroNome, segundoNome] = nomes;
+
+console.log(primeiroNome);
+console.log(segundoNome);
+
+//Spread Operator
+let primeiros = [1 , 2, 3];
+
+let numeros = [...primeiros, 4, 5, 10];
+
+console.log(numeros);
+
+let pessoa = {
+    nome:  "Matheus",
+    idade: 45,
+    cargo: "RH"
+};
+
+let novaPessoa = {
+    ...pessoa,
+    status: "Ativo",
+    cidade: "Campo Grande / MS",
+    telefone: "(18)9029-14924"
+};
+
+console.log(novaPessoa);
+
+function novoUsuario(info) {
+    let dados = {
+        ...info,
+        status: "Ativo",
+        inicio: "20/03/2023",
+        codigo: "123123"
+    };
+
+    console.log(dados);
+}
+
+novoUsuario({ nome: "Jose", sobrenome: "Silva", cargo: "DEV" });
+
+//Operador Rest
+function convidados(...nomes){
+    console.log("Seja Bem Vindo todos convidados")
+    console.log(nomes);
+}
+
+convidados("Matheus", "Lucas", "Maria");
+
+function sorteador(...numeros){
+    console.log(numeros);
+
+    const numeroGerado = Math.floor(Math.random() * numeros.length);
+
+    console.log("Número gerado foi: " + numeros[numeroGerado]);
+}
+
+sorteador(1, 4, 5, 15, 25, 90, 55, 34)
+
+//Operações em um array
+//Map
+
+let lista = ["Matheus", "Jose", "Maria"];
+
+lista.map((item, index) => {
+    console.log(`Passando: ${item} - Está na posição ${index} `);
+});
+
+//Operador Reduce = busca reduzir um array
+let numeros = [5, 3, 2 ];
+
+let total = numeros.reduce((acumulador, numero, indice, original) => {
+    console.log(`${acumulador} - Total até o momento`);
+    console.log(`${numero} - Valor autal`);
+    console.log(`${indice} - indice atual`);
+    console.log(`${original} - array original`);
+
+    return acumulador += numero;
+});
+
+console.log("Total do Reduce: " + total);
+
+//Operador Find
+
+let listagem = [5, 3, "José", 2, "Matheus"]
+
+let busca = listagem.find((item) => {
+    if(item === "José"){
+        return console.log("Item encontrado com sucesso");
+    }
+});
 */
+
+//Operador Filter
+
+let palavras = ["Matheus", "Ana", "José", "Ricardo Silva", "Sujeito Programador"];
+
+let resultado = palavras.filter((item) => {
+    return item.length <= 4;
+})
+ console.log(resultado);
+
+
 
 
 
